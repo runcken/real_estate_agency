bc3a63000c7c9a0603f3acce4b00aa9c4928ff1a
runcken <runcken@gmail.com>
2025-09-11 01:26:20 +0300

    fill with data field Owner.flats, migrate Flat.owners_phonenumber to Owner.phonenumber

--- property/admin.py
+++ property/admin.py
@@ -12,8 +12,7 @@ class FlatAdmin(admin.ModelAdmin):
         'new_building',
         'construction_year',
         'town',
-        'owners_phonenumber',
-        'owner_pure_phone']
+    ]
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
--- property/models.py
+++ property/models.py
@@ -6,13 +6,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField(
-        region='RU',
-        blank=True,
-        null=True,
-        verbose_name='Нормализованный номер владельца'
-    )
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -125,7 +118,6 @@ class Owner(models.Model):
     )
     flats = models.ManyToManyField(
         Flat,
-        null=True,
         verbose_name='Квартиры в собственности',
         related_name='owners'
         )
